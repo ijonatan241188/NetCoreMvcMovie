7433c77974dc72675bd999a67529815dd1025707
ijontan241188 <jonatandaniel.ismael@gmail.com>
2022-05-19 23:54:25 -0300

    Hoy

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -15,15 +15,18 @@ namespace MvcMovie.Models
 
         public int Id { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
 
         [Display(Name = "Release Date")]
-
         [DataType(DataType.Date)]
-
         public DateTime ReleaseDate { get; set; }
-
+       
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
 
 
@@ -31,6 +34,9 @@ namespace MvcMovie.Models
 
         public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
 
     }
